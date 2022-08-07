3a71bf1097c0bb3af1f0bfd56d13609d56719040
malierbay <malierbay9@gmail.com>
2022-08-07 22:28:26 +0300

    hw5

--- homework5/utility/CapabilityPaths.java
+++ homework5/utility/CapabilityPaths.java
@@ -2,8 +2,8 @@ package homework5.utility;
 
 public enum CapabilityPaths {
     //Bu enum ile capability içeren json dosyalarının pathlerine ulaşabiliyoruz.
-    CAPSET1("C:\\Users\\malie\\IdeaProjects\\homework5-malierbay9\\homework5\\resources\\caps\\oreo.json"),
-    CAPSET2("C:\\Users\\malie\\IdeaProjects\\homework5-malierbay9\\homework5\\resources\\caps\\kitkat.json");
+    CAPSET1("C:\\Users\\malie\\IdeaProjects\\homework5-malierbay9\\homework5\\resources\\caps\\testPhone.json"),
+    CAPSET2("C:\\Users\\malie\\IdeaProjects\\homework5-malierbay9\\homework5\\resources\\caps\\testPhone2.json");
 
     String path;
 
